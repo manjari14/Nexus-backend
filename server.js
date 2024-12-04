eca75744dd12738dc9770f9d2e406617af1e3c22
Manjari14 <sheoranmanjari@gmail.com>
2024-12-04 17:32:10 +0530

    changes

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ connectDB();
 
 app.use("/api", registrationRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
